722634c3976192f3836adcde36426dd91eda5ee9
abdelrazak27 <113916211+abdelrazak27@users.noreply.github.com>
2023-11-18 16:13:21 +0100

    (feat): ajouter un produit amélioré
    Améliorations type de données, changements bdd, et affichage liste des produits

--- src/pages/AdminPage.jsx
+++ src/pages/AdminPage.jsx
@@ -1,14 +1,18 @@
+import { useEffect, useState } from 'react';
 import useProduct from '../hooks/useProduct';
-import { supabase } from '../supabaseClient'; // Pour pouvoir utiliser l'API de Supabase
+import { supabase } from '../supabaseClient'; // Pour pouvoir utiliser l'API de supabase
 
 function AdminPage() {
-    const { product, setProduct } = useProduct();
+    const { product, setProduct, calculateTTC, calculateRemainingStock } = useProduct(); // produit à ajouter (hook)
+    const [products, setProducts] = useState([]); // liste des produits existants
 
+    // Changement dans le formulaire
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setProduct({ ...product, [name]: value }); // évite davoir un setter pour chaque élément du produit, on va chercher dans l'objet ce qu'on souhaite modifié
+        setProduct({ ...product, [name]: value }); // évite davoir un setter pour chaque élément du produit, on va chercher dans l'objet, ce qu'on souhaite modifier
     };
 
+    // Soumission du formulaire
     const handleSubmit = async (e) => {
         e.preventDefault();
         const { error } = await supabase
@@ -18,19 +22,54 @@ function AdminPage() {
                     name: product.name,
                     price_ht: parseFloat(product.priceHT),
                     tax_rate: parseFloat(product.taxRate),
+                    price_ttc: parseFloat(calculateTTC()),
                     stock_ordered: parseInt(product.stockOrdered),
-                    stock_maximum_available: parseInt(product.stockMaximumAvailable)
+                    stock_maximum_available: parseInt(product.stockMaximumAvailable),
+                    stock_available: parseInt(calculateRemainingStock()),
                 }
             ]);
         if (error) {
             console.error('Erreur lors de l\'ajout du produit :', error); // pour debug
         } else {
             console.log('Produit ajouté avec succès'); // pour debug
+            await fetchProducts();
+        }
+    };
+
+    // A chaque chargement du composant/de la page, on récupère les éléments de la table "products"
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    // Récupération des produits dans la BDD
+    const fetchProducts = async () => {
+        const { data, error } = await supabase
+            .from('products')
+            .select('*');
+        if (error) {
+            console.error('Erreur lors de la récupération des produits:', error);
+        } else {
+            setProducts(data);
         }
     };
 
     return (
         <div>
+            <h2>Liste des produits</h2>
+            <ul>
+                {products.map((productItem) => (
+                    <li key={productItem.id}>
+                        {productItem.name}<br/>Prix HT: {productItem.price_ht}€
+                        <br/>Pourcentages taxes: {productItem.tax_rate *100}%
+                        <br/>Prix TTC: {productItem.price_ttc}€
+                        <br/>Stock max: {productItem.stock_maximum_available}
+                        <br/>Stock actuel: {productItem.stock_available}
+                        <br/>Déjà acheté: {productItem.stock_ordered}
+
+                        <hr/>
+                    </li>
+                ))}
+            </ul>
             <h2>Ajouter un produit</h2>
             <form onSubmit={handleSubmit}>
                 <input
