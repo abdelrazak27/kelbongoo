9897ac072fdf1a412fe42c4fb1d34c6e881cac9e
abdelrazak27 <113916211+abdelrazak27@users.noreply.github.com>
2023-11-19 13:44:30 +0100

    (feat): addItem() ajouté
    Prenant compte des attributs demandés

--- src/hooks/useItem.js
+++ src/hooks/useItem.js
@@ -3,7 +3,13 @@ import { useState } from "react";
 const useItem = () => {
     const [items, setItems] = useState([]);
 
-    return { items, setItems };
+    // ajouter un produit dans le panier
+    const addItem = (cartId, productId, quantity) => {
+        const newItem = { cart_id: cartId, product_id: productId, quantity };
+        setItems([...items, newItem]);
+    };
+
+    return { items, setItems, addItem };
 }
 
 export default useItem;
