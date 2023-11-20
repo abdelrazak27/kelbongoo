07d4b50d24161dd504267443926881cfc732bb68
abdelrazak27 <113916211+abdelrazak27@users.noreply.github.com>
2023-11-20 18:27:34 +0100

    explication sécurité variables données bdd

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,5 +1,8 @@
 import mysql from 'mysql';
 
+// A stocker dans des variables d'environnement côté serveur de préférence pour plus de protection (Vercel offre 12 fichiers dans /api/* possible dans sa version gratuite, donc impossible à ce niveau du projet)
+// L'idée est que les informations ne doivent pas être côté client (visible pas l'utilisateur)
+// Cependant, il s'agit d'un test, mais je suis bien conscient du problème de sécurité de faire de cette façon
 const config = {
     host: 'mysql-kelbongoo.alwaysdata.net',
     user: 'kelbongoo',
