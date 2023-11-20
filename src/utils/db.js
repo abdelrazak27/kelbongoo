8a6dd0d5ea967ea07f15609afae2055176e2c38e
abdelrazak27 <113916211+abdelrazak27@users.noreply.github.com>
2023-11-20 18:32:22 +0100

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -3,6 +3,8 @@ import mysql from 'mysql';
 // A stocker dans des variables d'environnement côté serveur de préférence pour plus de protection (Vercel offre 12 fichiers dans /api/* possible dans sa version gratuite, donc impossible à ce niveau du projet)
 // L'idée est que les informations ne doivent pas être côté client (visible pas l'utilisateur)
 // Cependant, il s'agit d'un test, mais je suis bien conscient du problème de sécurité de faire de cette façon
+
+// autre solution : laisser la répition dans chacun des fichiers API en y indiquant les informations avec process.env.XXX
 const config = {
     host: 'mysql-kelbongoo.alwaysdata.net',
     user: 'kelbongoo',
